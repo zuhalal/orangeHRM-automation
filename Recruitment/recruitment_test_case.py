1e8b22e135fa68309d7423f1643fd8525518c1a9
zuhalal <zuhalalimulhadi19@gmail.com>
2022-07-31 10:44:30 +0700

    Add recruitment feature test case

---
+++ Recruitment/recruitment_test_case.py
@@ -0,0 +1,58 @@
+### Created by Zuhal 'Alimul Hadi
+from lib2to3.pgen2.driver import Driver
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import Select
+
+#variable
+username="Admin"
+password="admin123"
+url="https://opensource-demo.orangehrmlive.com"
+
+class TestRecruitment(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+    
+    def login(self, driver):
+        driver.find_element(By.NAME, "txtUsername").send_keys(username)
+        time.sleep(0.5)
+        driver.find_element(By.NAME, "txtPassword").send_keys(password)
+        time.sleep(0.5)
+        driver.find_element(By.NAME,"Submit").click()
+        time.sleep(3)
+
+    def test_download_resume(self):
+        driver = self.driver
+        driver.get(url)
+        time.sleep(3)
+
+        self.login(driver)
+
+        driver.find_element(By.ID, "menu_recruitment_viewRecruitmentModule").click()
+        time.sleep(3)
+        driver.find_element(By.ID, "menu_recruitment_viewCandidates").click()
+        time.sleep(3)
+
+        Select(driver.find_element(By.ID, "candidateSearch_jobVacancy")).select_by_value("1")
+        Select(driver.find_element(By.ID, "candidateSearch_status")).select_by_value("SHORTLISTED")
+        time.sleep(1)
+        driver.find_element(By.ID, "btnSrch").click()
+        time.sleep(3)
+
+        element = driver.find_elements(By.CSS_SELECTOR, ".odd .left a")
+
+        for el in element:
+            if el.text == "Download":
+                el.click()
+                self.assertIn(el.text, "Download")
+        
+        time.sleep(2)
+
+    def tearDown(self): 
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
