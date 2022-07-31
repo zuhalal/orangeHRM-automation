6c0272be96d91b6a527f430553e9b0b179a9d40f
zuhalal <zuhalalimulhadi19@gmail.com>
2022-07-31 09:16:55 +0700

    Fix: template

--- template.py
+++ template.py
@@ -8,11 +8,20 @@ from webdriver_manager.chrome import ChromeDriverManager
 #variable
 username="Admin"
 password="admin123"
+url="https://opensource-demo.orangehrmlive.com"
 
 class TestTemplate(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
     
+    def login(self, driver):
+        driver.find_element(By.NAME, "txtUsername").send_keys(username)
+        time.sleep(0.5)
+        driver.find_element(By.NAME, "txtPassword").send_keys(password)
+        time.sleep(0.5)
+        driver.find_element(By.NAME,"Submit").click()
+        time.sleep(3)
+
     def tearDown(self): 
         self.driver.close()
 
